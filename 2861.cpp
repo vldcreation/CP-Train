c603d2cd3c1ab8ad7071e5b1d07347a3cebd8b9d
Vicktor Desrony <vldcreation21@gmail.com>
2021-03-14 12:06:12 +0700

    SOL-URI-2861.CPP

---
+++ 2861.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    string inp,dummy;
+    cin>>n;
+    getline(cin,dummy);
+    while(n--){
+        getline(cin,inp);
+        cout<<"gzuz\n";
+    }
+ 
+    return 0;
+}
